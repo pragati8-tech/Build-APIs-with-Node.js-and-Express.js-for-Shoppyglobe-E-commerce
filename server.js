82d96206c526ccc325ade1c0e7aa838fc7939cf6
pragatiagrawal69 <your.email@example.com>
2026-06-13 13:04:40 +0530

    MongoDB Atlas connection setup

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // server.js
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db')
 
 dotenv.config();
-
+connectDB()
 const app = express();
 
 // Middleware - JSON for body parse
